e4066c2ba0e9b99ebdddc16820f757813628b664
Harshith R <harshith.raju0710@gmail.com>
2024-09-13 19:08:49 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from "react"
+import useTheme, { ThemeProvider } from "./context/theme"
+import ThemeButton from "./components/ThemeButton";
+import Card from "./components/Card";
+
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  // the name of these functions should be same as the one in the value of the ThemeProvider
+  const lightTheme = () => {
+    setThemeMode("light");
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  }
+
+  // to implement actual change in the theme
+  useEffect(() => {
+    document.querySelector('html').classList.remove('light', 'dark');
+    document.querySelector('html').classList.add(themeMode);
+  }, [themeMode])
+
   return (
-    <>
-     <h1 className="bg-blue-500 text-white">React Context API practice 2</h1>
-    </>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+              <ThemeButton />        
+          </div>
+          <div className="w-full max-w-sm mx-auto">
+              <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
